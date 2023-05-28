ed624faed50b88727b53d0e0c72f7806501f68c2
Per Kristian Engebretsen <per.engebretsen@lindbak.no>
2023-05-28 14:37:09 +0200

    Setup layout, render menu when button is clicked

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script>
   import '../app.css';
   import Header from '../components/header.svelte';
+  import MainNavigation from '../components/mainNavigation.svelte';
+
+  let showMenu = false;
+  const toggleMenu = () => (showMenu = !showMenu);
 </script>
 
-<div class="h-full flex flex-col bg-brand-beige font-light font-serif">
-  <Header />
+<div class="relative h-full flex flex-col bg-brand-beige font-light font-serif">
+  <Header {toggleMenu} />
+  {#if showMenu}
+    <div
+      on:mousedown={() => (showMenu = false)}
+      class="absolute top-0 left-0 h-full w-full bg-black opacity-60 cursor-pointer"
+    />
+    <MainNavigation {toggleMenu} />
+  {/if}
   <main class="grow-1 h-full">
     <slot />
   </main>
